07db639789ff5231f1937a904c4070e06ed0d18f
Randy <randy708ochoa@gmail.com>
2023-05-06 20:55:14 -0600

    Acortados los decimales si son mas de 3, ya no se permite poner 2 operadores seguidos

--- src/js/index.js
+++ src/js/index.js
@@ -18,16 +18,36 @@ class CALCULADORA {
     }
 
     actualizarPantalla = (valor) => {
+        const regex = {
+            operadores: /[*+-/]/,
+        }
+        
         if(pantalla.textContent == "0") {
             pantalla.textContent = ""
         }
         
         pantalla.textContent += valor.value
+
+        let ultimoCaracter = pantalla.textContent.charAt(pantalla.textContent.length - 1)
+        let penultimoCaracter = pantalla.textContent.charAt(pantalla.textContent.length - 2)
+
+        if(regex.operadores.test(penultimoCaracter) && regex.operadores.test(ultimoCaracter)) {
+            pantalla.textContent = pantalla.textContent.slice(0, -2).concat(pantalla.textContent.slice(-1))
+
+        }
+
         this.valorActual = pantalla.textContent
     }
 
     calcular = () => {
-        pantalla.textContent = eval(pantalla.textContent)
+        let resultado = eval(pantalla.textContent)
+        
+        if(Number.isInteger(resultado)){
+            pantalla.textContent = resultado
+        } else{
+            pantalla.textContent = resultado.toFixed(3).replace(/\.?0+$/, '')
+        }
+        
         this.valorActual = pantalla.textContent
     }
 }
@@ -36,7 +56,6 @@ const pantalla = document.querySelector(".calculadora__pantalla")
 const botones = document.getElementsByClassName("botones__boton")
 const botonesArray = Array.from(botones)
 const calculadora = new CALCULADORA()
-
 
 botonesArray.forEach(boton => {
     boton.addEventListener("click", () => {
